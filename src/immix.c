98bc71bf3975fb0290f1f3835434ef06d35ef0b3
Julien Portalier <julien@portalier.com>
2022-06-10 11:23:39 +0200

    Fix: GC mutex should allow reentrancy
    Because we can start a collect while trying to allocate memory.
    
    A better fix could be to unlock or have distinct locks (e.g.
    rwlock) because there is no telling that the GC collector fiber
    will run in the same thread that started the collection!

--- src/immix.c
+++ src/immix.c
@@ -52,7 +52,7 @@ void GC_init() {
     }
     pthread_mutexattr_t attr;
     pthread_mutexattr_init(&attr);
-    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE); // => ERRORCHECK
     pthread_mutexattr_setrobust(&attr, PTHREAD_MUTEX_ROBUST);
     pthread_mutex_init(GC_mutex, &attr);
 
